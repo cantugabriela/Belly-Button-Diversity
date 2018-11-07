3590c49c5aba980b9703a8fc3dd1faa144a25e00
gcantu <39506132+cantugabriela@users.noreply.github.com>
2018-11-07 15:04:17 -0600

    Advanced Challenge Completed
    Added gauge

--- Belly-Button-Biodiversity/static/js/app.js
+++ Belly-Button-Biodiversity/static/js/app.js
@@ -13,14 +13,73 @@ function buildMetadata(sample) {
   d3.json(metadataURL).then(function (data) {
     Object.entries(data).forEach(([key, value]) => {
       panelMetadata.append("h6").text(`${key}: ${value}`
-        // BONUS: Build the Gauge Chart
-        // buildGauge(data.WFREQ);
-
       );
     })
+
+    // BONUS: Build the Gauge Chart
+// buildGauge(data.WFREQ);
+  // Enter a speed between 0 and 180
+    var level = data.WFREQ;
+
+    // Trig to calc meter point
+    var degrees = 180 - (level*20),
+         radius = .7;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+         pathX = String(x),
+         space = ' ',
+         pathY = String(y),
+         pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    
+    var data = [{ type: 'scatter',
+       x: [0], y:[0],
+        marker: {size: 28, color:'850000'},
+        showlegend: false,
+        name: 'speed',
+        text: level,
+        hoverinfo: 'text+name'},
+      { values: [45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 45/8, 50],
+      rotation: 90,
+      text: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3',
+                '1-2', '0-1', ''],
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors:['#84B589','rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                             'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                             'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                             '#F4F1E4','#F8F3EC', 'rgba(255, 255, 255, 0)',]},
+      labels: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3',
+      '1-2', '0-1', ''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+    
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+
+      title: 'Belly Button Wash Frequency',
+      xaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]}
+    };
+    Plotly.newPlot('gauge', data, layout);
   })
 }
-
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
@@ -60,6 +119,7 @@ Plotly.newPlot('bubble', trace1, layout);
       showlegend: true,
     };
     Plotly.newPlot('pie', data, layout);
+
   }
 )}
 
